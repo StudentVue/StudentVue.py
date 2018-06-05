9fe9e13249deb273d8f43dd8f22f1e45475bc48f
Kai Chang <kaijchang@gmail.com>
2018-06-05 13:08:46 -0700

    scraper at package level

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='studentvue',
     packages=['studentvue'],
-    version='1',
+    version='1.11',
     description='Python Scraper for StudentVue Portals',
     author='Kai Chang',
     url='https://github.com/kajchang/studentvuescraper',
