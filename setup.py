a21efc6bddc32c72bc673c1c5b26996bf8e5d22d
Kai Chang <kaijchang@gmail.com>
2019-08-06 15:37:03 -0700

    reformat setup.py

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ setup(
     author='Kai Chang',
     url='https://github.com/kajchang/studentvuescraper',
     license='MIT',
+    long_description_content_type='text/markdown',
     long_description=open(os.path.join(os.path.abspath(
         os.path.dirname(__file__)), 'README.md')).read(),
-    long_description_content_type="text/markdown",
     install_requires=[open(os.path.join(os.path.abspath(
         os.path.dirname(__file__)), 'requirements.txt')).read().split('\n')[:-1]]
 )
