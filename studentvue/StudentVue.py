ea38e8bbddca5eab7a31dd9ac9ea4f20cc18ee71
Kai Chang <kaijchang@gmail.com>
2020-03-22 11:07:14 -0700

    add docs

--- studentvue/StudentVue.py
+++ studentvue/StudentVue.py
@@ -24,6 +24,22 @@ class StudentVue:
                  zeep_transport: zeep.Transport = None,
                  zeep_settings: zeep.Settings = None,
                  debug: bool = False):
+        """
+        :param username: student's username
+        :type username: str
+        :param password: student's password
+        :type password: str
+        :param district_domain: domain that the  school district hosts StudentVue on
+        :type district_domain: str
+        :param xmljson_serializer: (optional) serializer used to serialize xml to json
+        :type xmljson_serializer: xmljson.XMLData
+        :param zeep_transport: (optional) custom zeep client transport
+        :type zeep_transport: zeep.Transport
+        :param zeep_settings: (optional) custom zeep client settings
+        :type zeep_settings: zeep.Settings
+        :param debug: if set to true, turns on debug logging from zeep
+        :type debug: bool
+        """
         if debug:
             self._setup_debug()
         else:
@@ -47,6 +63,98 @@ class StudentVue:
             transport=zeep_transport,
             settings=zeep_settings
         )
+
+    def get_messages(self) -> OrderedDict:
+        """
+        :return: student's messages
+        :rtype: OrderedDict
+        """
+        return self._xml_json_serialize(self._make_service_request('GetPXPMessages'))
+
+    def get_calendar(self) -> OrderedDict:
+        """
+        :return: student's assignments / events calendar
+        :rtype: OrderedDict
+        """
+        return self._xml_json_serialize(self._make_service_request('StudentCalendar'))
+
+    def get_attendance(self) -> OrderedDict:
+        """
+        :return: student's attendance
+        :rtype: OrderedDict
+        """
+        return self._xml_json_serialize(self._make_service_request('Attendance'))
+
+    def get_gradebook(self, report_period: int = 0) -> OrderedDict:
+        """
+        :param report_period: (optional) report period to fetch gradebook for
+        :type report_period: int
+        :return: student's gradebook for the specified report period
+        :rtype: OrderedDict
+        """
+        return self._xml_json_serialize(self._make_service_request('Gradebook', ReportPeriod=report_period))
+
+    def get_class_notes(self) -> OrderedDict:
+        """
+        :return: student's class notes
+        :rtype: OrderedDict
+        """
+        return self._xml_json_serialize(self._make_service_request('StudentHWNotes'))
+
+    def get_student_info(self) -> OrderedDict:
+        """
+        :return: student's information
+        :rtype: OrderedDict
+        """
+        return self._xml_json_serialize(self._make_service_request('StudentInfo'))
+
+    def get_schedule(self, term_index: int = 0) -> OrderedDict:
+        """
+        :param term_index: (optional) term index to fetch schedule for
+        :type term_index: int
+        :return: student's schedule for the specified term
+        :rtype: OrderedDict
+        """
+        return self._xml_json_serialize(self._make_service_request('StudentClassList', TermIndex=term_index))
+
+    def get_school_info(self) -> OrderedDict:
+        """
+        :return: student's school information
+        :rtype: OrderedDict
+        """
+        return self._xml_json_serialize(self._make_service_request('StudentSchoolInfo'))
+
+    def list_report_cards(self) -> OrderedDict:
+        """
+        :return: list of student's report cards
+        :rtype: OrderedDict
+        """
+        return self._xml_json_serialize(self._make_service_request('GetReportCardInitialData'))
+
+    def get_report_card(self, document_guid: str) -> OrderedDict:
+        """
+        :param document_guid: id of the report card to fetch, found using :func:`~studentvue.StudentVue.list_report_cards`
+        :type document_guid: str
+        :return: content of the specified report card
+        :rtype: OrderedDict
+        """
+        return self._xml_json_serialize(self._make_service_request('GetReportCardDocumentData', DocumentGU=document_guid))
+
+    def list_documents(self) -> OrderedDict:
+        """
+        :return: list of student's documents
+        :rtype: OrderedDict
+        """
+        return self._xml_json_serialize(self._make_service_request('GetStudentDocumentInitialData'))
+
+    def get_document(self, document_guid: str) -> OrderedDict:
+        """
+        :param document_guid: id of the document to fetch, found using :func:`~studentvue.StudentVue.list_documents`
+        :type document_guid: str
+        :return: content of the specified document
+        :rtype: OrderedDict
+        """
+        return self._xml_json_serialize(self._make_service_request('GetContentOfAttachedDoc', DocumentGU=document_guid))
 
     @staticmethod
     def _suppress_warnings():
@@ -100,39 +208,3 @@ class StudentVue:
 
     def _xml_json_serialize(self, xml_string: str) -> OrderedDict:
         return self.xmljson_serializer.data(etree.fromstring(xml_string))
-
-    def get_messages(self) -> OrderedDict:
-        return self._xml_json_serialize(self._make_service_request('GetPXPMessages'))
-
-    def get_calendar(self) -> OrderedDict:
-        return self._xml_json_serialize(self._make_service_request('StudentCalendar'))
-
-    def get_attendance(self) -> OrderedDict:
-        return self._xml_json_serialize(self._make_service_request('Attendance'))
-
-    def get_gradebook(self, report_period: int = 0) -> OrderedDict:
-        return self._xml_json_serialize(self._make_service_request('Gradebook', ReportPeriod=report_period))
-
-    def get_class_notes(self) -> OrderedDict:
-        return self._xml_json_serialize(self._make_service_request('StudentHWNotes'))
-
-    def get_student_info(self) -> OrderedDict:
-        return self._xml_json_serialize(self._make_service_request('StudentInfo'))
-
-    def get_schedule(self, term_index: int = 0) -> OrderedDict:
-        return self._xml_json_serialize(self._make_service_request('StudentClassList', TermIndex=term_index))
-
-    def get_school_info(self) -> OrderedDict:
-        return self._xml_json_serialize(self._make_service_request('StudentSchoolInfo'))
-
-    def list_report_cards(self) -> OrderedDict:
-        return self._xml_json_serialize(self._make_service_request('GetReportCardInitialData'))
-
-    def get_report_card(self, document_guid: str) -> OrderedDict:
-        return self._xml_json_serialize(self._make_service_request('GetReportCardDocumentData', DocumentGU=document_guid))
-
-    def list_documents(self) -> OrderedDict:
-        return self._xml_json_serialize(self._make_service_request('GetStudentDocumentInitialData'))
-
-    def get_document(self, document_guid: str) -> OrderedDict:
-        return self._xml_json_serialize(self._make_service_request('GetContentOfAttachedDoc', DocumentGU=document_guid))
