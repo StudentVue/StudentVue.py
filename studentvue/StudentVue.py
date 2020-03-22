7d375781e03eb98d462173253f550328dffb1f36
Kai Chang <kaijchang@gmail.com>
2020-03-22 10:13:53 -0700

    implement known routes

--- studentvue/StudentVue.py
+++ studentvue/StudentVue.py
@@ -1,13 +1,29 @@
 import zeep
-import requests
 
+from lxml import etree
+from xmljson import abdera, XMLData
 from urllib.parse import urlparse
+
+from collections import OrderedDict
+
+
+class UnescapingPlugin(zeep.Plugin):
+    def egress(self, envelope, http_headers, operation, binding_options):
+        xml_string = etree.tostring(envelope).decode()
+        xml_string = xml_string.replace('&amp;', '&')
+        new_envelope = etree.fromstring(xml_string)
+        return new_envelope, http_headers
 
 
 class StudentVue:
     """The StudentVue API class"""
 
-    def __init__(self, username: str, password: str, district_domain: str, debug: bool = False):
+    def __init__(self,
+                 username: str, password: str, district_domain: str,
+                 xmljson_serializer: XMLData = abdera,
+                 zeep_transport: zeep.Transport = None,
+                 zeep_settings: zeep.Settings = None,
+                 debug: bool = False):
         if debug:
             self._setup_debug()
         else:
@@ -24,15 +40,12 @@ class StudentVue:
             if self.district_domain[len(self.district_domain) - 1] == '/':
                 self.district_domain = self.district_domain[:-1]
 
-        self.session = requests.Session()
-        self.session.headers.update({
-            'SOAPAction': 'http://edupoint.com/webservices/ProcessWebServiceRequest',
-            'User-Agent': 'StudentVUE/8.0.26 CFNetwork/1121.2.2 Darwin/19.3.0',
-            'Content-Type': 'text/xml'
-        })
+        self.xmljson_serializer = xmljson_serializer
         self.client = zeep.Client(
             'https://{0}/Service/PXPCommunication.asmx?WSDL'.format(self.district_domain),
-            transport=zeep.transports.Transport(session=self.session)
+            plugins=[UnescapingPlugin()],
+            transport=zeep_transport,
+            settings=zeep_settings
         )
 
     @staticmethod
@@ -67,11 +80,11 @@ class StudentVue:
             }
         })
 
-    def _make_service_request(self, method_name, **kwargs):
+    def _make_service_request(self, method_name, **kwargs) -> str:
         param_str = '&lt;Parms&gt;'
         for key, value in kwargs.items():
             param_str += '&lt;' + key + '&gt;'
-            param_str += value
+            param_str += str(value)
             param_str += '&lt;/' + key + '&gt;'
         param_str += '&lt;/Parms&gt;'
 
@@ -84,3 +97,24 @@ class StudentVue:
             methodName=method_name,
             paramStr=param_str
         )
+
+    def _xml_json_serialize(self, xml_string: str) -> OrderedDict:
+        return self.xmljson_serializer.data(etree.fromstring(xml_string))
+
+    def get_messages(self) -> OrderedDict:
+        return self._xml_json_serialize(self._make_service_request('GetPXPMessages'))
+
+    def get_calendar(self) -> OrderedDict:
+        return self._xml_json_serialize(self._make_service_request('StudentCalendar'))
+
+    def get_attendance(self) -> OrderedDict:
+        return self._xml_json_serialize(self._make_service_request('Attendance'))
+
+    def get_gradebook(self, report_period: int = 0) -> OrderedDict:
+        return self._xml_json_serialize(self._make_service_request('Gradebook', ReportPeriod=report_period))
+
+    def get_class_notes(self) -> OrderedDict:
+        return self._xml_json_serialize(self._make_service_request('StudentHWNotes'))
+
+    def get_student_info(self) -> OrderedDict:
+        return self._xml_json_serialize(self._make_service_request('StudentInfo'))
